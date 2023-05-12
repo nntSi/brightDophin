7af3061fe18c12c1353b233bd42c1f9c974522da
Wachirawit Sihawong <wachrawit_si@kkumail.com>
2023-05-12 15:08:38 +0700

    scoroll auto

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,19 @@ function App() {
   return (
     <div className="min-h-screen bg-white/90">
       <NavBar/>
+      <div id="a" className="h-[500px] bg-gray-600">
+
+      </div>
+      <div id="b" className="h-[500px] bg-red-400">
+
+      </div>
+      <div id="c" className="h-[500px] bg-yellow-200">
+
+      </div>
+      <div id="d" className="h-[500px]">
+
+      </div>
+    
     </div>
   );
 }
--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -1,30 +1,33 @@
+import { motion, useScroll } from "framer-motion";
 import logo from "../../assets/image/manud.png";
+import { Link } from "react-scroll";
 export default function NavBar() {
   return (
-    <div className={`h-16 sticky`}>
-      <div className="flex h-full shadow-sm justify-between items-center lg:px-64 px-4">
-        <div className="flex items-center">
-          <img className="w-10 h-10" src={logo} />
-          {/* <p className="font-bold ml-3 text-slate-800">EARTH</p> */}
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 2 }} className="sticky top-0 z-50 bg-white">
+      <div className="h-16">
+        <div className="flex h-full shadow-sm justify-between items-center lg:px-64 px-4">
+          <div className="flex items-center">
+            <img className="w-10 h-10" src={logo} />
+          </div>
+          <div className="hidden lg:flex space-x-8 items-center">
+            <Link to="a" smooth={true} duration={500} className="text-sm text-gray-700 duration-200 hover:text-primary-clr">
+              Home
+            </Link>
+            <Link to="b" smooth={true} duration={500} className="text-sm text-gray-700 duration-200 hover:text-primary-clr">
+              Our Project
+            </Link>
+            <Link to="c" smooth={true} duration={500} className="text-sm text-gray-700 duration-200 hover:text-primary-clr">
+              Our Services
+            </Link>
+            <Link to="d" smooth={true} duration={500} className="text-xs font-semibold text-white px-6 py-2 rounded-3xl shadow-sm bg-primary-clr duration-200 hover:opacity-80 ">
+              Sign In
+            </Link>
+          </div>
+          <button className="rounded lg:hidden hover:text-primary-clr duration-200">
+            <i className="bx bx-menu text-2xl"></i>
+          </button>
         </div>
-        <div className="hidden lg:flex space-x-8 items-center">
-          <button className="text-sm text-gray-700 duration-200 hover:text-primary-clr">
-            Home
-          </button>
-          <button className="text-sm text-gray-700 duration-200 hover:text-primary-clr">
-            Our Project
-          </button>
-          <button className="text-sm text-gray-700 duration-200 hover:text-primary-clr">
-            Our Services
-          </button>
-          <button className="text-xs font-semibold text-white px-6 py-2 rounded-3xl shadow-sm bg-primary-clr duration-200 hover:opacity-80 ">
-            Sign In
-          </button>
-        </div>
-        <button className="rounded lg:hidden hover:text-primary-clr duration-200">
-          <i className="bx bx-menu text-2xl"></i>
-        </button>
       </div>
-    </div>
+    </motion.div>
   );
 }
